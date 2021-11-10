7999d014e0c144eeb52ab535ca7c74f817201c98
JENNY SHEA <jenny_shea1933@gmail.com>
2021-11-10 16:45:57 -0500

    Nov 10 changes

---
+++ Weather.py
@@ -0,0 +1,50 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "88eaaddb",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#imports pandas and shorten the name\n",
+    "import pandas as pd\n",
+    "\n",
+    "#import numpy so you can use arrays and shorten the name\n",
+    "import numpy as np\n",
+    "\n",
+    "#import matplotlib.pyplot so you can make plots and charts and shorten the name\n",
+    "import matplotlib.pyplot as pyplot\n",
+    "\n",
+    "#import requests so you can make API calls\n",
+    "import requests\n",
+    "\n",
+    "#import time so you can use dates and timepoints\n",
+    "import time\n",
+    "\n",
+    "#import json \n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:PythonData]",
+   "language": "python",
+   "name": "conda-env-PythonData-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.11"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
